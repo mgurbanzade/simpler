f19ebcf87050a033529d6fd06c339d2946dcb689
Mahram Gurbanzade <mgurbanzade@yahoo.com>
2018-04-17 19:56:42 +0400

    middleware

--- lib/simpler/controller.rb
+++ lib/simpler/controller.rb
@@ -54,7 +54,7 @@ module Simpler
     end
 
     def params
-      @request.params.update(@request.env['simpler.params'])
+      @request.env['simpler.params'].merge!(@request.params)
     end
 
     def plain(text)
